92da5d9620ab2cdaf1d7d9bbfc00412ea140b2df
NHJeans <guswo1013@naver.com>
2024-05-23 01:40:53 +0900

    Feat: standard ex2 제출

---
+++ react-5-standard-4st-assignment/src/components/Box.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+// TODO: props로 받은 $active 에 따라 배경색이 blue 또는 gray가 되도록 해보세요.
+const StyledBox = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: ${(props) => (props.$active ? "blue" : "gray")};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  color: white;
+  font-size: 20px;
+
+  //! hover는 연습입니다
+  &:hover {
+    background-color: ${(props) => (props.$active ? "darkblue" : "darkgray")};
+  }
+`;
+
+function Box({ $active, onClick }) {
+  return <StyledBox $active={$active} onClick={onClick} />;
+}
+
+export default Box;
---
+++ react-5-standard-4st-assignment/src/components/BoxContainer.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import Box from "./Box";
+import { useState } from "react";
+
+const Container = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+function BoxContainer() {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <Container>
+      {/* TODO: active prop 을 어떻게 해야 클릭한 박스임을 알수있을 지 active prop에 할당할 값을 수정해보세요. */}
+      {[0, 1, 2, 3, 4].map((index) => (
+        <Box
+          key={index}
+          $active={activeIndex === index}
+          onClick={() => handleClick(index)}
+        />
+      ))}
+    </Container>
+  );
+}
+
+export default BoxContainer;
