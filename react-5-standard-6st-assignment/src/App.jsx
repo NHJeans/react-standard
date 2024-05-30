0ec79614dd878d59bf49b8c2b0cd2ed2e4bf9dfb
NHJeans <guswo1013@naver.com>
2024-05-30 18:05:35 +0900

    Feat: customhooks 과제

---
+++ react-5-standard-6st-assignment/src/App.jsx
@@ -0,0 +1,21 @@
+import useFetch from "./hooks/useFetch";
+
+// TODO: useFetch 라는 커스텀훅을 사용해서 리팩터링 해보세요
+
+const App = () => {
+  const { title, loading, error } = useFetch(
+    "https://jsonplaceholder.typicode.com/todos/1"
+  );
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <h1>Error: {error.message}</h1>;
+
+  return (
+    <div>
+      <h1>Data Fetching Example</h1>
+      <p>{title}</p>
+    </div>
+  );
+};
+
+export default App;
